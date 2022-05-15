ba05f63a1c7c269d639d5ab2dd6c915427951c80
Bishal Shrestha <bishal.modern@gmail.com>
2022-05-15 09:11:54 +0545

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Code First Model
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
+//PackageManagerConsole
+//EF Core Tool - Nuget
+//add-migration AddCategoryToDatabase
+//update-database
+
